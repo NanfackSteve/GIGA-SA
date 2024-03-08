ac8e28702c69ca7014b5ab4feb651df858c6c2ec
Nanfack Steve7 <nanfacksteve7@gmail.com>
2024-03-08 13:24:03 +0100

    update error page

--- 404.php
+++ 404.php
@@ -1,25 +1,54 @@
-<?php $title_page = "Error 404 - Page Not Found" ?>
+<!DOCTYPE html>
+<html lang="fr">
 
-<?php ob_start(); ?>
+<head>
+    <meta charset="utf-8">
+    <title>"Erreur 404 - Page Not Found" </title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
 
-<!-- 404 Start -->
-<div class="container-xxl py-5 wow zoomIn" data-wow-delay="0.5s">
-    <div class="container text-center">
-        <div class="row justify-content-center">
-            <div class="col-lg-6">
-                <i class="bi bi-exclamation-triangle display-1 text-primary"></i>
-                <h1 class="display-1">404</h1>
-                <h1 class="mb-4">Page non trouvée</h1>
-                <p class="mb-4">Nous sommes désolés, la page que vous recherchez n’existe pas sur notre site !</p>
-                <a class="btn btn-primary py-3 px-5" href="index.php">Retourner à la Page d'accueil</a>
+    <!-- Favicon -->
+    <link href="img/icons/logo.jpg" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600&family=Teko:wght@400;500;600&display=swap" rel="stylesheet">
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <!-- 404 Start -->
+    <div class="container-xxl py-5 wow zoomIn" data-wow-delay="0.5s">
+        <div class="container text-center">
+            <div class="row justify-content-center">
+                <div class="col-lg-6">
+                    <i class="bi bi-exclamation-triangle display-1 text-primary"></i>
+                    <h1 class="display-1">404</h1>
+                    <h1 class="mb-4">Page non trouvée</h1>
+                    <p class="mb-4">Nous sommes désolés, la page que vous recherchez n’existe pas sur notre site !
+                    </p>
+                    <a class="btn btn-primary py-3 px-5" href="index.php">Retourner à la Page d'accueil</a>
+                </div>
             </div>
         </div>
     </div>
-</div>
-<!-- 404 End -->
+    <!-- 404 End -->
 
-<?php $content_page = ob_get_clean();
+</body>
 
-require_once('tmp_main_layout.php')
-
-?>
+</html>
